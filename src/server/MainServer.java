720dff595379d92968dff14ac8f2184fa316e2eb
Seyoon Park <psyoona@naver.com>
2017-05-10 23:39:24 +0900

    Ver 0.1 post String test

--- src/server/MainServer.java
+++ src/server/MainServer.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
+
 public class MainServer extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
@@ -24,34 +26,47 @@ public class MainServer extends HttpServlet {
 		process(req, resp);
 	}
 
-	
-	protected void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-		System.out.println(getBody(req));
+	protected void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String result = getBody(req);
+		String[] array = result.split("\"");
+		dumpArray(array);
+		System.out.println(result);
 	}
-	
-	public static String getBody(HttpServletRequest request) throws IOException {
+
+	public static void dumpArray(String[] array) {
+		for (int i = 0; i < array.length; i++)
+			if(array[i].equals("type")){
+				System.out.format("My type is %s", array[i+2]);
+//				System.out.println("My type is " + array[i+1]);
+			}
 		
+			//System.out.format("array[%d] = %s%n", i, array[i]);
+	}
+
+	public static String getBody(HttpServletRequest request) throws IOException {
+
 		String body = null;
 		StringBuilder stringBuilder = new StringBuilder();
 		BufferedReader bufferedReader = null;
-		
-		try{
+
+		try {
 			InputStream inputStream = request.getInputStream();
-			if(inputStream != null){
+			if (inputStream != null) {
 				bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 				char[] charBuffer = new char[128];
 				int bytesRead = -1;
-				while((bytesRead = bufferedReader.read(charBuffer)) > 0 ){
+				while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
 					stringBuilder.append(charBuffer, 0, bytesRead);
 				}
 			}
 		} catch (IOException ex) {
 			throw ex;
 		} finally {
-			if(bufferedReader != null){
-				try{
+			if (bufferedReader != null) {
+				try {
 					bufferedReader.close();
-				} catch (IOException ex){
+				} catch (IOException ex) {
 					throw ex;
 				}
 			}
