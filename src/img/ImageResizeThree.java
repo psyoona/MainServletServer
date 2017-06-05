c2683df410ca5b5f8cee46116370b6900889551a
Seyoon Park <psyoona@naver.com>
2017-06-06 07:43:41 +0900

    Ver 1.27 Resize Image Three Code Refactoring

--- src/img/ImageResizeThree.java
+++ src/img/ImageResizeThree.java
@@ -17,27 +17,11 @@ public class ImageResizeThree extends ImageResize{
 			image1 = ImageIO.read(new File(Constants.IMG_PATH + loginID + "/" +fileOne));
 			image2 = ImageIO.read(new File(Constants.IMG_PATH + loginID + "/" + fileTwo));
 			image3 = ImageIO.read(new File(Constants.IMG_PATH + loginID + "/" + fileThree));
-						
-			resizeImage1 = image1.getScaledInstance(Constants.RESIZEWIDTH3, Constants.RESIZEHEIGHT3, Image.SCALE_SMOOTH);
-			resizeImage2 = image2.getScaledInstance(Constants.RESIZEWIDTH3, Constants.RESIZEHEIGHT3, Image.SCALE_SMOOTH);
-			resizeImage3 = image3.getScaledInstance(Constants.RESIZEWIDTH3, Constants.RESIZEHEIGHT3, Image.SCALE_SMOOTH);			
-
-			newImage1 = new BufferedImage(Constants.RESIZEWIDTH3, Constants.RESIZEHEIGHT3, BufferedImage.TYPE_INT_RGB);
-			Graphics2D g = (Graphics2D) newImage1.getGraphics();
-			g.drawImage(resizeImage1, 0, 0, null);
-			newImage2 = new BufferedImage(Constants.RESIZEWIDTH3, Constants.RESIZEHEIGHT3, BufferedImage.TYPE_INT_RGB);
-			Graphics2D g2 = (Graphics2D) newImage2.getGraphics();
-			g2.drawImage(resizeImage2, 0, 0, null);
-			newImage3 = new BufferedImage(Constants.RESIZEWIDTH3, Constants.RESIZEHEIGHT3, BufferedImage.TYPE_INT_RGB);
-			Graphics2D g3 = (Graphics2D) newImage3.getGraphics();
-			g3.drawImage(resizeImage3, 0, 0, null);
-
-			g.dispose();
-			g2.dispose();
-			g3.dispose();
-			ImageIO.write(newImage1, "jpg", new File(Constants.IMG_PATH + loginID +"/album/" + fileOne));
-			ImageIO.write(newImage2, "jpg", new File(Constants.IMG_PATH + loginID + "/album/" + fileTwo));
-			ImageIO.write(newImage3, "jpg", new File(Constants.IMG_PATH + loginID + "/album/" + fileThree));
+			
+			Resacle.scale(image1, Constants.IMG_PATH + loginID +"/" + "album/" + fileOne,"jpg", Constants.RESIZEWIDTH3, Constants.RESIZEHEIGHT3);
+			Resacle.scale(image2, Constants.IMG_PATH + loginID +"/" + "album/" + fileTwo,"jpg", Constants.RESIZEWIDTH3, Constants.RESIZEHEIGHT3);
+			Resacle.scale(image3, Constants.IMG_PATH + loginID +"/" + "album/" + fileThree,"jpg", Constants.RESIZEWIDTH3, Constants.RESIZEHEIGHT3);
+			
 			ImageMergeThree imgmer = new ImageMergeThree(fileOne, fileTwo, fileThree, emotion, loginID);
 			result = imgmer.merge();
 
